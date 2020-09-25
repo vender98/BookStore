86c356b2235bed0c9e88d8a5b86475a46f3bbbda
Daniil Borisovskii <vender98@mail.ru>
2020-09-25 20:22:23 +0300

    Add Network API method to request books

--- app/src/main/java/com/vender98/bookstore/api/NetworkApi.kt
+++ app/src/main/java/com/vender98/bookstore/api/NetworkApi.kt
@@ -2,6 +2,7 @@ package com.vender98.bookstore.api
 
 import com.vender98.bookstore.BuildConfig
 import com.vender98.bookstore.dto.Response
+import com.vender98.bookstore.dto.books.BooksAndAuthors
 import com.vender98.bookstore.dto.profile.Profile
 import io.reactivex.Single
 import retrofit2.http.GET
@@ -10,5 +11,8 @@ interface NetworkApi {
 
     @GET("${BuildConfig.API_TOKEN}%2Fprofile.json?alt=media")
     fun getProfile(): Single<Response<Profile>>
+
+    @GET("${BuildConfig.API_TOKEN}%2Fbooks.json?alt=media")
+    fun getBooks(): Single<Response<BooksAndAuthors>>
 
 }
